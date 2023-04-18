14efdab206482fb270e9037fad8c7b3c2dbb8859
LeeHansHinLun <79551623+LeeHansHinLun@users.noreply.github.com>
2023-04-17 20:38:28 -0500

    I really don't know how

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-git submodule update --init && ./remap.sh && ./decompile.sh && ./init.sh && ./newApplyPatches.sh && mvn clean install
+git submodule update --init && ./remap.sh && ./decompile.sh && ./init.sh && ./newApplyPatches.sh
+cd FlamePaper-Server
+mvn clean install
+cd ../
+cd FlamePaper-API
+mvn clean install
